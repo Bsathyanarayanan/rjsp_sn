6b43e83fcd0c4f3090976ea5641ff2c326e99a80
Sathyanarayanan B <150320993+Bsathyanarayanan@users.noreply.github.com>
2024-03-01 17:34:53 -0800

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Navbar from "./components/Navbar/Navbar";
+import Laptop from './components/Laptop/Laptop';
+import Mobile from "./components/Mobile/Mobile";
+import Tab from './components/Tab/Tab';
+import Watchs from './components/Watchs/Watchs';
+import Home from './components/Home/Home';
+import Sign_In from './components/Sign/Sign_in';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+      <Navbar/>
+    
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/Mobile" element={<Mobile/>}/>
+        <Route path="/Laptop" element={<Laptop/>}/>
+        <Route path="/Tab" element={<Tab/>}/>
+        <Route path="/Watchs" element={<Watchs/>}/>
+        <Route path="/Sign_In" element={<Sign_In/>}/>
+      </Routes>
+     
+      </BrowserRouter>
+          <Footer/>
     </div>
+      
+    
   );
 }
 
